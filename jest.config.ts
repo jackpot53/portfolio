87e726420e4d557d2690da74461337b8c40d5438
john.k <john@johnkui-MacBookPro-2.local>
2026-05-08 10:54:31 +0900

    feat: add Jest + React Testing Library

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: { jsx: 'react-jsx' } }],
+  },
+  testMatch: ['**/__tests__/**/*.test.{ts,tsx}'],
+}
+
+export default config
